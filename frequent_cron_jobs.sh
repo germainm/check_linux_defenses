6e6d8a4b7b2c7ce2ec57147085c2ce65c2c08a3b
germainm <germainm@hotmail.com>
2021-05-30 08:25:57 -0400

    Create frequent_cron_jobs.sh

---
+++ frequent_cron_jobs.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#permet de saoir quels sont les tâches durant un intervalle de temps. Le principe est de ne pas lister les tâches qui ont été actives durant toute la période
+for i in $(seq 1 610); do ps -e --format cmd >> /tmp/monprocs.tmp; sleep 0.1; done; sort /tmp/monprocs.tmp | uniq -c | grep -v "\[" | sed '/^.\{200\}./d' | sort | grep -E -v "\s*[6-9][0-9][0-9]|\s*[0-9][0-9][0-9][0-9]"; rm /tmp/monprocs.tmp;
